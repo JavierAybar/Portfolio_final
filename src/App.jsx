55e7f989cc272182ebaab133eea82fd1e459f59e
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2024-03-06 13:34:43 -0300

    Add structure/ Import icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import './index.css';
+
+import StickyNavbar from './components/StickyNavbar'
+import { Projects }  from './components/Projects';
+import ScrollToBtn from './components/ScrollTopBtn'
+import Skills from './components/Skills'
+import Hero from './components/Hero';
+
+// import your icons
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import Footer from './components/Footer';
+
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world
-      </h1>
+    <div className='bg-black ' >
+      <StickyNavbar />
+      <Hero />
+      <Projects />
+      <Skills />
+      <Footer />
+      <ScrollToBtn />
     </div>
   )
 }
 
-export default App
+export default App;
+library.add(fab, fas, far)
